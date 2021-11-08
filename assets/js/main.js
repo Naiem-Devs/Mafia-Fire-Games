ccc25ab8584cdb60124ed39e0180b05b23213f88
Naiem_Devs <dnsnaiem@gmail.com>
2021-11-08 14:58:51 +0600

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,5 +7,37 @@
   }); 
 
   
+  const zoomElement = document.querySelector("#heroBg");
+  let zoom = 1;
+  const ZOOM_SPEED = 0.1;
+
+  document.addEventListener("scroll", function(e) {
+      if( window.scrollY < 100) {
+          zoomElement.style.transform=`scale(1) `
+      }
+      else if( window.scrollY < 200) {
+          zoomElement.style.transform=`scale(1.05) `
+      }
+      else if( window.scrollY < 300) {
+          zoomElement.style.transform=`scale(1.1) `
+      }
+      else if( window.scrollY < 400) {
+          zoomElement.style.transform=`scale(1.15) `
+      }
+      
+      else if( window.scrollY < 500) {
+          zoomElement.style.transform=`scale(1.2) `
+      }
+      else if( window.scrollY < 600) {
+          zoomElement.style.transform=`scale(1.25) `
+      }
+      else if( window.scrollY < 800) {
+          zoomElement.style.transform=`scale(1.3) `
+      }
+      else if( window.scrollY < 1000) {
+          zoomElement.style.transform=`scale(1.35) `
+      }
+
+  });
  
 })(jQuery);
