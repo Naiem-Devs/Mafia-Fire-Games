b6b8a1a6c012f86adcee3312e3488c91d73abfeb
Naiem_Devs <dnsnaiem@gmail.com>
2021-11-08 16:25:42 +0600

    update_V2

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,38 +6,36 @@
     $('.mobile-menu').toggleClass('siteBar');   
   }); 
 
+
+
+
+  let itemBtn = document.querySelectorAll('.Person_top li')
+  let personBtm = document.querySelector('.Person_btm')
+  let personBtmMbl = document.querySelector('.Person_btm_mbl')
+
   
-  const zoomElement = document.querySelector("#heroBg");
-  let zoom = 1;
-  const ZOOM_SPEED = 0.1;
 
-  document.addEventListener("scroll", function(e) {
-      if( window.scrollY < 100) {
-          zoomElement.style.transform=`scale(1) `
-      }
-      else if( window.scrollY < 200) {
-          zoomElement.style.transform=`scale(1.05) `
-      }
-      else if( window.scrollY < 300) {
-          zoomElement.style.transform=`scale(1.1) `
-      }
-      else if( window.scrollY < 400) {
-          zoomElement.style.transform=`scale(1.15) `
-      }
-      
-      else if( window.scrollY < 500) {
-          zoomElement.style.transform=`scale(1.2) `
-      }
-      else if( window.scrollY < 600) {
-          zoomElement.style.transform=`scale(1.25) `
-      }
-      else if( window.scrollY < 800) {
-          zoomElement.style.transform=`scale(1.3) `
-      }
-      else if( window.scrollY < 1000) {
-          zoomElement.style.transform=`scale(1.35) `
-      }
+console.log(itemBtn);
+  itemBtn.forEach(itemBtns => {
+      itemBtns.addEventListener('click', () => {
+          let itemBtnsCont = itemBtns.querySelector('a')
+          removeActiveClases()
+          personBtm.setAttribute('data-id', itemBtnsCont.textContent )
+          personBtmMbl.setAttribute('data-id', itemBtnsCont.textContent )
+          itemBtns.classList.add('active')
+      })
+  })
 
-  });
+  function removeActiveClases() {
+      itemBtn.forEach(itemBtns => {
+          itemBtns.classList.remove('active')
+      })
+  }
+
+
+
+
+
+
  
 })(jQuery);
